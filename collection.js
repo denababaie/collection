55acb90f334d9725f632d1a92642568e95cdc9c8
denababaie <72995019+denababaie@users.noreply.github.com>
2020-11-10 16:57:23 +0100

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,75 @@
+const collection = [
+    {
+      title: 'Get Out',
+      author: 'Jordan Peele',
+      category: ['Horror'],
+      link: 'https://www.youtube.com/watch?v=DzfpyUB60YY&ab_channel=MovieclipsTrailers',
+      description:  'Get Out follows Chris, a young African-American man who uncovers a disturbing secret when he meets the family of his white girlfriend, Rose.'
+    },
+  
+    {
+        title: 'Persepolis ',
+        author: 'Marjane Satrapi',
+        category: ['Animated'],
+        link: 'https://www.youtube.com/watch?v=3PXHeKuBzPY&t=47s&ab_channel=SonyPicturesClassics' ,
+        description:  'The story follows a young girl as she comes of age against the backdrop of the Iranian Revolution.'
+    
+      },
+
+      {
+        title: 'Her ',
+        author: 'Spike Jonze',
+        category: ['Romantic'],
+        link: 'https://www.youtube.com/watch?v=6QRvTv_tpw0&t=55s&ab_channel=MovieclipsComingSoon',
+        description:  'In a near future, a lonely writer develops an unlikely relationship with an operating system designed to meet his every need.'
+    
+      },
+
+
+      {
+        title: 'Lady Bird ',
+        author: 'Greta Gerwig',
+        category: ['Comedy'],
+        link: 'https://www.youtube.com/watch?v=cNi_HC839Wo&ab_channel=A24',
+        description:  'In 2002, an artistically inclined seventeen-year-old girl comes of age in Sacramento, California.'
+    
+      },
+
+      {
+        title: 'Parasite',
+        author: 'Bong Joon Ho ',
+        category: ['Thriller'],
+        link: 'https://www.youtube.com/watch?v=5xH0HfJHsaY&t=13s&ab_channel=IGN',
+        description:  'Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan',
+    
+      },
+
+
+
+      {
+        title: 'Spirited Away',
+        author: 'Hayao Miyazaki ',
+        category: ['Animated'],
+        link: 'Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan',
+        description:  'In 2002, an artistically inclined seventeen-year-old girl comes of age in SacrameGreed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.'
+    
+      },
+
+      {
+        title: 'Spirited Away',
+        author: 'Hayao Miyazaki ',
+        category: ['Animated'],
+        link: 'Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan',
+        description:  'In 2002, an artistically inclined seventeen-year-old girl comes of age in SacrameGreed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.'
+    
+      },
+      
+      {
+        title: 'Gone Girl',
+        author: 'David Fincher ',
+        category: ['Thriller'],
+        link: 'https://www.youtube.com/watch?v=2-_-1nJf8Vg&ab_channel=20thCenturyStudios',
+        description:  'With his wifes disappearance having become the focus of an intense media circus, a man sees the spotlight turned on him when it's suspected that he may not be innocent.'
+    
+      },
+]
